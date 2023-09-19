9c0eb2edcfd3a3394fdd349a409514d9d3426ef0
projecteinf <projecteinf@boscdelacoma.cat>
2023-09-19 11:33:19 -0600

    Creada classe producte

---
+++ Model/Producte.cs
@@ -0,0 +1,10 @@
+namespace mba.Articles
+{
+    public class Producte
+    {
+        public int Id { get; set; }
+        public string Nom { get; set; }
+        public string Descripcio { get; set; }
+        
+    }
+}
